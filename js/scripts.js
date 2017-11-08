94dfa5869ed8121033daf37c9056e5f22d387d31
murungiKirima <murungikirima@gmail.com>
2017-11-08 17:41:23 +0300

    Add js

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,15 @@ function Player(playerName, turnScore, totalScore) {
 }
 
 Player.prototype.roll = function() {
-  var diceVal = [1, 2, 3, 4, 5, 6];
-  var rollVal = diceValues[Math.floor(Math.random() * diceValues.length)];
-  if (rollVal === 1) {
+  var diceValues = [1, 2, 3, 4, 5, 6];
+  var rollValues = diceValues[Math.floor(Math.random() * diceValues.length)];
+  if (rollValues === 1) {
     this.turnScore = 0;
   }
    else {
-    this.turnScore = this.turnScore + rollVal;
+    this.turnScore = this.turnScore + rollValues;
   };
-  return rollVal;
+  return rollValues;
 }
 
 Player.prototype.score = function() {
@@ -21,23 +21,16 @@ Player.prototype.score = function() {
   this.turnScore = 0;
 }
 
-//user interface
 
-//button to launch game
 $("#start").click(function(event){
-  $(".header1").fadeIn(1000);
+  $(".header1").fadeIn(900);
 });
-//click to register players and start game.
+
 $("#btn-register").click(function(event){
 event.preventDefault();
-  // $("#well-play1").show(900);
-  // $("#well-play2").show(900);
-  // $("h5").show(1200);
-  // $("#name-0").show(300);
-  // $("#name-1").show(300);
-  // $(".game-panel1").show(600);
-  // $(".game-panel2").show(700);
-  // $("#new-game-register").fadeOut(400);
+  $(".header2").fadeIn(900);
+
+
 
   var player1Name = $("#player1").val();
   var player2Name = $("#player2").val();
@@ -46,19 +39,18 @@ event.preventDefault();
 
 
   $("#player1-name").text(player1.playerName);
-  $("#player1-total-score").html("<span class='player1-total-score'>" + player1.totalScore + "</span>");
+  $("#total-score-1").html("<span class='player1-total-score'>" + player1.totalScore + "</span>");
 
-  $("#btn-roll").click(function(event) {
+  $("#player1-roll").click(function(event) {
     event.preventDefault();
     var player1RolledNumber = player1.roll();
     if (player1RolledNumber === 1) {
-      $(".player1-scored1").show(500).fadeOut(1000);
-      $("#well-play1").fadeOut(900);
-      $("#well-play2").show(900);
+      alert("Better luck time")
     }
-    $(".player1-rolled-number").text(player1RolledNumber);
-    $(".player1-turn-score").text(player1.turnScore);
+    $("#die-roll-1").text(player1RolledNumber);
+    $("#total-score-1").text(player1.turnScore);
   });
+
 
   $("#btn-hold").click(function(event) {
     event.preventDefault();
@@ -67,28 +59,18 @@ event.preventDefault();
     $(".player1-rolled-number").text("");
     $(".player1-turn-score").text(player1.turnScore);
     if (player1.totalScore >= 100) {
-      $("#game-section").fadeOut(400);
-      $(".player1-victory").show().fadeOut(1200);
-    } else {
-
-    $(".player2-scored1").hide();
-    $("#well-play2").show(900);
-    $("#well-play1").hide(900);
-    $(".hold").show().fadeOut(1500);
+      alert("You won GAME OVER");
     }
-
   });
 
   $(".player2-name").text(player2.playerName);
   $(".player2-total-score").html("<span class='player2-total-score'>" + player2.totalScore + "</span>");
 
-  $("#btn-roll1").click(function(event) {
+  $("player2-roll").click(function(event) {
     event.preventDefault();
     var player2RolledNumber = player2.roll();
     if (player2RolledNumber === 1) {
-      $(".player2-scored1").show().fadeOut(1000);
-      $("#well-play2").fadeOut(1000);
-      $("#well-play1").show(900);
+
     }
     $(".player2-rolled-number").text(player2RolledNumber);
     $(".player2-turn-score").text(player2.turnScore);
@@ -101,12 +83,9 @@ event.preventDefault();
     $(".player2-rolled-number").text("");
     $(".player2-turn-score").text("");
     if (player2.totalScore >= 100) {
-      $("#game-section").fadeOut(400);
-      $(".player2-victory").show().fadeOut(1200);
+
     } else {
-    $("#well-play1").show(900);
-    $(".hold").show().fadeOut(1500);
-    $("#well-play2").hide(900);
+
 
     }
   });
