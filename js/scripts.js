552c89bd4d949393a7beb9573b357f5305dae139
murungiKirima <murungikirima@gmail.com>
2017-11-08 14:40:59 +0300

    Change css folder name

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,114 @@
-// window.onscroll = function() {scrollFunction()};
-//
-// function scrollFunction() {
-//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//         document.getElementById("myBtn").style.display = "block";
-//     } else {
-//         document.getElementById("myBtn").style.display = "none";
-//     }
-// }
-//
-// // When the user clicks on the button, scroll to the top of the document
-// function topFunction() {
-//     document.body.scrollTop = 0; // For Chrome, Safari and Opera
-//     document.documentElement.scrollTop = 0; // For IE and Firefox
-// }
-$(function(){
-	var dice = $("#dice");
-	dice.click(function(){
-		$(".wrap").append("<div id='dice_mask'></div>");//add mask
-		dice.attr("class","dice");//After clearing the last points animation
-		dice.css('cursor','default');
-		var num = Math.floor(Math.random()*6+1);//random num 1-6
-		dice.animate({left: '+2px'}, 100,function(){
-			dice.addClass("dice_t");
-		}).delay(200).animate({top:'-2px'},100,function(){
-			dice.removeClass("dice_t").addClass("dice_s");
-		}).delay(200).animate({opacity: 'show'},600,function(){
-			dice.removeClass("dice_s").addClass("dice_e");
-		}).delay(100).animate({left:'-2px',top:'2px'},100,function(){
-			dice.removeClass("dice_e").addClass("dice_"+num);
-			$("#result").html("Your throwing points are<span>"+num+"</span>");
-			dice.css('cursor','pointer');
-			$("#dice_mask").remove();//remove mask
-		});
-	});
+function Player(playerName, turnScore, totalScore) {
+  this.playerName = playerName;
+  this.turnScore = turnScore;
+  this.totalScore = totalScore;
+}
+
+Player.prototype.roll = function() {
+  var diceVal = [1, 2, 3, 4, 5, 6];
+  var rollVal = diceValues[Math.floor(Math.random() * diceValues.length)];
+  if (rollVal === 1) {
+    this.turnScore = 0;
+  }
+   else {
+    this.turnScore = this.turnScore + rollVal;
+  };
+  return rollVal;
+}
+
+Player.prototype.score = function() {
+  this.totalScore = this.turnScore + this.totalScore;
+  this.turnScore = 0;
+}
+
+//user interface
+
+//button to launch game
+$("#start").click(function(event){
+  $(".header1").fadeIn(1000);
+});
+//click to register players and start game.
+$("#btn-register").click(function(event){
+event.preventDefault();
+  // $("#well-play1").show(900);
+  // $("#well-play2").show(900);
+  // $("h5").show(1200);
+  // $("#name-0").show(300);
+  // $("#name-1").show(300);
+  // $(".game-panel1").show(600);
+  // $(".game-panel2").show(700);
+  // $("#new-game-register").fadeOut(400);
+
+  var player1Name = $("#player1").val();
+  var player2Name = $("#player2").val();
+  var player1 = new Player (player1Name,0,0)
+  var player2 = new Player (player2Name,0,0)
+
+
+  $("#player1-name").text(player1.playerName);
+  $("#player1-total-score").html("<span class='player1-total-score'>" + player1.totalScore + "</span>");
+
+  $("#btn-roll").click(function(event) {
+    event.preventDefault();
+    var player1RolledNumber = player1.roll();
+    if (player1RolledNumber === 1) {
+      $(".player1-scored1").show(500).fadeOut(1000);
+      $("#well-play1").fadeOut(900);
+      $("#well-play2").show(900);
+    }
+    $(".player1-rolled-number").text(player1RolledNumber);
+    $(".player1-turn-score").text(player1.turnScore);
+  });
+
+  $("#btn-hold").click(function(event) {
+    event.preventDefault();
+    player1.score();
+    $(".player1-total-score").text(player1.totalScore);
+    $(".player1-rolled-number").text("");
+    $(".player1-turn-score").text(player1.turnScore);
+    if (player1.totalScore >= 100) {
+      $("#game-section").fadeOut(400);
+      $(".player1-victory").show().fadeOut(1200);
+    } else {
+
+    $(".player2-scored1").hide();
+    $("#well-play2").show(900);
+    $("#well-play1").hide(900);
+    $(".hold").show().fadeOut(1500);
+    }
+
+  });
+
+  $(".player2-name").text(player2.playerName);
+  $(".player2-total-score").html("<span class='player2-total-score'>" + player2.totalScore + "</span>");
+
+  $("#btn-roll1").click(function(event) {
+    event.preventDefault();
+    var player2RolledNumber = player2.roll();
+    if (player2RolledNumber === 1) {
+      $(".player2-scored1").show().fadeOut(1000);
+      $("#well-play2").fadeOut(1000);
+      $("#well-play1").show(900);
+    }
+    $(".player2-rolled-number").text(player2RolledNumber);
+    $(".player2-turn-score").text(player2.turnScore);
+  });
+
+  $("#btn-hold1").click(function(event) {
+    event.preventDefault();
+    player2.score();
+    $(".player2-total-score").text(player2.totalScore);
+    $(".player2-rolled-number").text("");
+    $(".player2-turn-score").text("");
+    if (player2.totalScore >= 100) {
+      $("#game-section").fadeOut(400);
+      $(".player2-victory").show().fadeOut(1200);
+    } else {
+    $("#well-play1").show(900);
+    $(".hold").show().fadeOut(1500);
+    $("#well-play2").hide(900);
+
+    }
+  });
+
 });
